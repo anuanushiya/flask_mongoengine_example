352c59a8f6ea12cef30b84c5778bc8991c53af4b
J. Cohen <hostelmarketing@gmail.com>
2015-12-06 18:30:31 -0800

    Add more validation. Add to docstrings.

--- models.py
+++ models.py
@@ -8,14 +8,20 @@ me.connect(DATABASE_NAME)
 
 
 class Page(me.Document):
-    """Represents a page."""
+    """Represents a page.
+
+    This is an example of defining a fixed schema with validation.
+    """
     title = me.StringField(required=True)
     author = me.StringField(required=True)
-    body = me.StringField()
+    body = me.StringField(required=True)
     created_at = me.DateTimeField(default=datetime.now)
 
 
 class Profile(me.Document):
-    """Represents a Github user profile."""
+    """Represents a Github user profile.
+
+    This is an example of saving raw JSON data into Mongo with no constraints.
+    """
     raw_json = me.DynamicField()
     downloaded_at = me.DateTimeField(default=datetime.now)
